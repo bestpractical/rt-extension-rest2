d20ddd0dbe95201c93335f30beb56a426f91fd63
Thomas Sibley <trs@bestpractical.com>
2013-08-01 17:51:25 -0700

    Provide user Privileged status as a boolean flag

--- lib/RTx/REST/Resource/User.pm
+++ lib/RTx/REST/Resource/User.pm
@@ -9,6 +9,14 @@ extends 'RTx::REST::Resource';
 with 'RTx::REST::Resource::Role::Record';
 with 'RTx::REST::Resource::Role::Record::DisableOnDelete';
 
+around 'serialize_record' => sub {
+    my $orig = shift;
+    my $self = shift;
+    my $data = $self->$orig(@_);
+    $data->{Privileged} = $self->record->Privileged ? 1 : 0;
+    return $data;
+};
+
 sub forbidden {
     my $self = shift;
     return 0 if not $self->record->id;
