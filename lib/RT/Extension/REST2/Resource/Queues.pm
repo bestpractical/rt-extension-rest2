ca1b6b658da62a508402d571e6f3a9f79a6e52c2
Shawn M Moore <shawn@bestpractical.com>
2017-07-13 22:05:59 +0000

    Add /queues/all endpoint
    The /queues endpoint, and other searches, produce no results by default,
    in line with RT's behavior of collections with no ->Limits produce
    no records.

--- lib/RT/Extension/REST2/Resource/Queues.pm
+++ lib/RT/Extension/REST2/Resource/Queues.pm
@@ -13,6 +13,15 @@ sub dispatch_rules {
         regex => qr{^/queues/?$},
         block => sub { { collection_class => 'RT::Queues' } },
     ),
+    Path::Dispatcher::Rule::Regex->new(
+        regex => qr{^/queues/all/?$},
+        block => sub {
+            my ($match, $req) = @_;
+            my $queues = RT::Queues->new($req->env->{"rt.current_user"});
+            $queues->UnLimit;
+            return { collection => $queues };
+        },
+    ),
 }
 
 __PACKAGE__->meta->make_immutable;
