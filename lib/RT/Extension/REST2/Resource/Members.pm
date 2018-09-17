83c71d6251e6da02c174e409c7286771bf554c5d
gibus <gibus@easter-eggs.com>
2018-09-17 11:18:24 +0200

    Add endpoints to operate on group members
    * Getting direct members of a group: GET /group/:id/members
    * Getting direct and undirect members of a group: GET /group/:id/deepmembers
    * Getting user members of a group: GET /group/:id/groupmembers?recursively=[01]
    * Getting group members of a group: GET /group/:id/usermembers?recursively=[01]
    * Removing all members of a group: DELETE /group/:id/member/:id
    * Removing a member from a group: DELETE /group/:id/members
    * Adding members to a group: PUT /group/:id/members passing JSON array of principals ids

---
+++ lib/RT/Extension/REST2/Resource/Members.pm
@@ -0,0 +1,136 @@
+package RT::Extension::REST2::Resource::Members;
+use strict;
+use warnings;
+
+use Moose;
+use namespace::autoclean;
+
+extends 'RT::Extension::REST2::Resource::Collection';
+with 'RT::Extension::REST2::Resource::Role::RequestBodyIsJSON' =>
+  {type => 'ARRAY'};
+
+has 'group' => (
+    is  => 'ro',
+    isa => 'RT::Group',
+);
+
+sub dispatch_rules {
+    Path::Dispatcher::Rule::Regex->new(
+        regex => qr{^/group/(\d+)/(deep|group|user)?members/?$},
+        block => sub {
+            my ($match, $req) = @_;
+            my $group_id = $match->pos(1);
+            my $type = $match->pos(2) || '';
+            my $group = RT::Group->new($req->env->{"rt.current_user"});
+            $group->Load($group_id);
+
+            if ($type eq 'deep') {
+                return {group => $group, collection => $group->DeepMembersObj};
+            } elsif ($type eq 'group') {
+                return {group => $group, collection => $group->GroupMembersObj(Recursively => $req->parameters->{recursively} // 1)};
+            } elsif ($type eq 'user') {
+                return {group => $group, collection => $group->UserMembersObj(Recursively => $req->parameters->{recursively} // 1)};
+            } else {
+                return {group => $group, collection => $group->MembersObj};
+            }
+        },
+    ),
+    Path::Dispatcher::Rule::Regex->new(
+        regex => qr{^/group/(\d+)/member/(\d+)/?$},
+        block => sub {
+            my ($match, $req) = @_;
+            my $group_id = $match->pos(1);
+            my $member_id = $match->pos(2) || '';
+            my $group = RT::Group->new($req->env->{"rt.current_user"});
+            $group->Load($group_id);
+            my $collection = $group->MembersObj;
+            $collection->Limit(FIELD => 'MemberId', VALUE => $member_id);
+            return {group => $group, collection => $collection};
+        },
+    ),
+}
+
+sub forbidden {
+    my $self = shift;
+    return 0 if $self->current_user->HasRight(
+        Right   => "AdminUsers",
+        Object  => RT->System,
+    );
+    return 1;
+}
+
+sub serialize {
+    my $self = shift;
+    my $collection = $self->collection;
+    my @results;
+
+    while (my $item = $collection->Next) {
+        my ($id, $class);
+        if (ref $item eq 'RT::GroupMember' || ref $item eq 'RT::CachedGroupMember') {
+            my $principal = $item->MemberObj;
+            $class = $principal->IsGroup ? 'group' : 'user';
+            $id = $principal->id;
+        } elsif (ref $item eq 'RT::Group') {
+            $class = 'group';
+            $id = $item->id;
+        } else {
+            $class = 'user';
+            $id = $item->id;
+        }
+
+        my $result = {
+            type => $class,
+            id   => $id,
+            _url => RT::Extension::REST2->base_uri . "/$class/$id",
+        };
+        push @results, $result;
+    }
+    return {
+        count       => scalar(@results)         + 0,
+        total       => $collection->CountAll    + 0,
+        per_page    => $collection->RowsPerPage + 0,
+        page        => ($collection->FirstRow / $collection->RowsPerPage) + 1,
+        items       => \@results,
+    };
+}
+
+sub allowed_methods {
+    my @ok = ('GET', 'HEAD', 'DELETE', 'PUT');
+    return \@ok;
+}
+
+sub content_types_accepted {[{'application/json' => 'from_json'}]}
+
+sub delete_resource {
+    my $self = shift;
+    my $collection = $self->collection;
+    while (my $group_member = $collection->Next) {
+        $RT::Logger->info('Delete ' . ($group_member->MemberObj->IsGroup ? 'group' : 'user') . ' ' . $group_member->MemberId . ' from group '.$group_member->GroupId);
+        $group_member->GroupObj->Object->DeleteMember($group_member->MemberId);
+    }
+    return 1;
+}
+
+sub from_json {
+    my $self   = shift;
+    my $params = JSON::decode_json($self->request->content);
+    my $group = $self->group;
+
+    my $method = $self->request->method;
+    my @results;
+    if ($method eq 'PUT') {
+        for my $param (@$params) {
+            if ($param =~ /^\d+$/) {
+                push @results, $group->AddMember($param);
+            } else {
+                push @results, [0, 'You should provide principal id for each member to add'];
+            }
+        }
+    }
+    $self->response->body(JSON::encode_json(\@results));
+    return;
+}
+
+__PACKAGE__->meta->make_immutable;
+
+1;
