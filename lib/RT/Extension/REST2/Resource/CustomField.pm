a66c978c83c24a36435ab4813ad2407b92d7bd0f
gibus <gibus@easter-eggs.com>
2018-10-08 13:28:33 +0200

    Add all CRUD operations for CustomField

--- lib/RT/Extension/REST2/Resource/CustomField.pm
+++ lib/RT/Extension/REST2/Resource/CustomField.pm
@@ -7,7 +7,9 @@ use namespace::autoclean;
 
 extends 'RT::Extension::REST2::Resource::Record';
 with 'RT::Extension::REST2::Resource::Record::Readable',
-     'RT::Extension::REST2::Resource::Record::Hypermedia';
+     'RT::Extension::REST2::Resource::Record::Hypermedia',
+     'RT::Extension::REST2::Resource::Record::DeletableByDisabling',
+     'RT::Extension::REST2::Resource::Record::Writable';
 
 sub dispatch_rules {
     Path::Dispatcher::Rule::Regex->new(
@@ -18,6 +20,21 @@ sub dispatch_rules {
         regex => qr{^/customfield/(\d+)/?$},
         block => sub { { record_class => 'RT::CustomField', record_id => shift->pos(1) } },
     )
+}
+
+sub forbidden {
+    my $self = shift;
+    my $method = $self->request->method;
+    if ($self->record->id) {
+        if ($method eq 'GET') {
+            return !$self->record->CurrentUserHasRight('SeeCustomField');
+        } else {
+            return !($self->record->CurrentUserHasRight('SeeCustomField') && $self->record->CurrentUserHasRight('AdminCustomField'));
+        }
+    } else {
+        return !$self->current_user->HasRight(Right => "AdminCustomField", Object => RT->System);
+    }
+    return 0;
 }
 
 __PACKAGE__->meta->make_immutable;
