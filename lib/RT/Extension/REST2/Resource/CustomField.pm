cd580e351bc359c09ed9b4610a0a9f57431e60f9
gibus <gibus@easter-eggs.com>
2018-10-09 13:56:39 +0200

    Add customfieldvalues endpoints in hypermedia links of customfield

--- lib/RT/Extension/REST2/Resource/CustomField.pm
+++ lib/RT/Extension/REST2/Resource/CustomField.pm
@@ -7,7 +7,8 @@ use namespace::autoclean;
 
 extends 'RT::Extension::REST2::Resource::Record';
 with 'RT::Extension::REST2::Resource::Record::Readable',
-     'RT::Extension::REST2::Resource::Record::Hypermedia';
+     'RT::Extension::REST2::Resource::Record::Hypermedia'
+        => { -alias => { hypermedia_links => '_default_hypermedia_links' } };
 
 sub dispatch_rules {
     Path::Dispatcher::Rule::Regex->new(
@@ -18,6 +19,19 @@ sub dispatch_rules {
         regex => qr{^/customfield/(\d+)/?$},
         block => sub { { record_class => 'RT::CustomField', record_id => shift->pos(1) } },
     )
+}
+
+sub hypermedia_links {
+    my $self = shift;
+    my $links = $self->_default_hypermedia_links(@_);
+
+    if ($self->record->IsSelectionType) {
+        push @$links, {
+            ref  => 'customfieldvalues',
+            _url => RT::Extension::REST2->base_uri . "/customfield/" . $self->record->id . "/values",
+        };
+    }
+    return $links;
 }
 
 __PACKAGE__->meta->make_immutable;
